648678dd05aedd3eaa52d31aacac530c7f8c702f
aska diliya <askadiliyan@gmail.com>
2025-11-12 22:47:22 +0700

    feat : add subtraction

--- divide.js
+++
@@ -1,6 +0,0 @@
-function divide(num1,num2){
-    console.log(num1/num2)
-}
-divide(4,2)
-
-//ex ghifar
--- increase.js
+++
@@ -1,4 +0,0 @@
-function increaseNumber(num1, num2) {
-  return num1 + num2;
-}
-console.log(increaseNumber(2, 5));
